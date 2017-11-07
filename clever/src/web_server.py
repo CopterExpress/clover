fe624b7f5963d69bc6826c1b3845644ce14a5344
Oleg Kalachev <okalachev@gmail.com>
2017-11-07 15:38:25 +0300

    Simple web-server node for serving Clever interface

---
+++ clever/src/web_server.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import rospy
+from flask import Flask, send_from_directory, send_file
+
+
+rospy.init_node('web_server', disable_signals=True)
+
+
+port = rospy.get_param('~port', 8080)
+host = rospy.get_param('~host', '0.0.0.0')
+serve_path = rospy.get_param('~path')
+app = Flask(__name__)
+
+
+@app.route('/')
+def serve_index():
+    return send_from_directory(serve_path, 'index.html')
+
+
+@app.route('/<path:path>')
+def serve_static(path):
+    print serve_path, path
+    return send_from_directory(serve_path, path)
+
+
+rospy.loginfo('Serving on %s:%s', host, port)
+app.run(host=host, port=port, threaded=True)
