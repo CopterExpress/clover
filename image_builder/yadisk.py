a65a50ffd3d39b91593776cb4e2b0aa4785a339e
urpylka <urpylka@gmail.com>
2018-04-24 15:00:29 +0300

    image_builder: change ini to json in yadisk.py
    Signed-off-by: Artem Smirnov <urpylka@gmail.com>

--- image_builder/yadisk.py
+++ image_builder/yadisk.py
@@ -9,7 +9,7 @@
 #
 
 from YaDiskClient.YaDiskClient import YaDisk
-import os.path, sys
+import os.path, sys, json
 
 def upload(_login, _password, _server_dir, _file):
     if os.path.isfile(_file):
@@ -36,14 +36,10 @@ def main():
         
         if os.path.isfile(sys.argv[1]) and os.path.isfile(sys.argv[2]):
 
-            from ConfigParser import SafeConfigParser
-            cfgParser = SafeConfigParser()
-            cfgParser.read(sys.argv[1])
-            # print "login: " + cfgParser.get('yadisk','login')
-            # print "password: " + cfgParser.get('yadisk','password')
-            # print "server_dir: " + cfgParser.get('yadisk','server_dir')
+            with open('coex-ci.json') as json_data:
+                d = json.load(json_data)
+                upload(d['yadisk']['login'], d['yadisk']['password'], d['yadisk']['server_dir'], sys.argv[2])
 
-            upload(cfgParser.get('yadisk','login'), cfgParser.get('yadisk','password'), cfgParser.get('yadisk','server_dir'), sys.argv[2])
         else:
             print "Error: file-path or config-path is bad"
             return 1
