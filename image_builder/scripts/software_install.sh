7e84853c379f96c14a5eaf43ba8932791a726c78
Artem Smirnov <urpylka@gmail.com>
2018-06-19 13:39:37 +0300

    image_builder: small echo fix

--- image_builder/scripts/software_install.sh
+++ image_builder/scripts/software_install.sh
@@ -6,7 +6,7 @@ set -e
 # Image software installation
 ##################################################################################################################################
 
-echo -e "\033[0;31m\033[1m$(date) | #1 Network installing\033[0m\033[0m"
+echo -e "\033[0;31m\033[1m$(date) | #1 Software installing\033[0m\033[0m"
 
 # TODO: Use dnsmasq instead of isc-dhcp-server
 apt-get install --no-install-recommends -y \
@@ -25,4 +25,4 @@ apt-get install --no-install-recommends -y \
   ipython3 \
   python3-pip
 
-echo -e "\033[0;31m\033[1m$(date) | End of network installation\033[0m\033[0m"
+echo -e "\033[0;31m\033[1m$(date) | End of software installation\033[0m\033[0m"
