fec4859cfe1aa3df84d8861ac7c9bdd73536160f
Smirnov Artem <urpylka@users.noreply.github.com>
2018-07-06 22:10:58 +0300

    image_builder: enable the lirc-rpi module for uart

--- image_builder/scripts/hardware_setup.sh
+++ image_builder/scripts/hardware_setup.sh
@@ -38,6 +38,7 @@ echo -e "\033[0;31m\033[1m$(date) | #6 Turn on UART\033[0m\033[0m"
 # https://github.com/RPi-Distro/raspi-config/pull/75
 /usr/bin/raspi-config nonint do_serial 1
 /usr/bin/raspi-config nonint set_config_var enable_uart 1 /boot/config.txt
+/usr/bin/raspi-config nonint set_config_var dtoverlay pi3-miniuart-bt /boot/config.txt
 
 # After adding to Raspbian OS
 # https://github.com/RPi-Distro/raspi-config/commit/d6d9ecc0d9cbe4aaa9744ae733b9cb239e79c116
