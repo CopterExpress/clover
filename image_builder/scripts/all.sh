8b60d2c61da7d73923f7d8a917ce8ef89ad63dac
Artem Smirnov <urpylka@gmail.com>
2018-08-21 21:46:31 +0300

    image_builder: delete unused script

--- image_builder/scripts/all.sh
+++
@@ -1,5 +0,0 @@
-./change_boot_part.sh
-./init_image.sh qemu_build_17082018 raspbian_nov_2017
-./software_install.sh
-./network_setup.sh
-./ros_install.sh https://github.com/CopterExpress/clever.git master True
