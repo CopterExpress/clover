4850d0382500b342e3cdfecaf18fb0dd768b8954
Artem Smirnov <urpylka@gmail.com>
2018-08-21 17:59:52 +0300

    image_builder: use IMAGE_PATH in publish image

--- image_builder/image_config.sh
+++ image_builder/image_config.sh
@@ -216,49 +216,52 @@ umount_system() {
 }
 
 publish_image() {
-  # TEMPLATE: publish_image_bash $BUILD_DIR $IMAGE_NAME $YA_SCRIPT $CONFIG_FILE $RELEASE_ID $RELEASE_BODY
+  # TEMPLATE: publish_image_bash <IMAGE_PATH> <YA_SCRIPT> <CONFIG_FILE> <RELEASE_ID> <RELEASE_BODY>
 
   # https://developer.github.com/v3/repos/releases/
 
+  IMAGE_NAME=$(basename $1)
+  BUILD_DIR=$(dirname $1)
+
   echo_stamp "Zip image"
-  if [ ! -e "$1/$2.zip" ];
+  if [ ! -e "${BUILD_DIR}/${IMAGE_NAME}.zip" ];
   then
-    cd $1 && zip $2.zip $2
+    cd ${BUILD_DIR} && zip ${IMAGE_NAME}.zip ${IMAGE_NAME}
     echo_stamp "Zipping complete!" "SUCCESS"
   else
     echo_stamp "Zip-archive already created"
-    cd $1 && rm $2.zip && zip $2.zip $2 \
+    cd ${BUILD_DIR} && rm ${IMAGE_NAME}.zip && zip ${IMAGE_NAME}.zip ${IMAGE_NAME} \
     && echo_stamp "Old archive was deleted & create new" "SUCCESS"
   fi
 
   echo_stamp "Upload image"
-  local IMAGE_LINK=$($3 $4 $1/$2.zip) \
+  local IMAGE_LINK=$($2 $3 ${BUILD_DIR}/${IMAGE_NAME}.zip) \
   && echo_stamp "Upload copmlete!" "SUCCESS"
 
   echo_stamp "Meashure size of zip-image"
-  local IMAGE_SIZE=$(du -sh $1/$2.zip | awk '{ print $1 }') \
+  local IMAGE_SIZE=$(du -sh ${BUILD_DIR}/${IMAGE_NAME}.zip | awk '{ print $1 }') \
   && echo_stamp "Meashuring copmlete!" "SUCCESS"
 
   echo_stamp "Meashure hash-sum of zip-image"
-  local IMAGE_HASH=$(sha256sum $1/$2.zip | awk '{ print $1 }') \
+  local IMAGE_HASH=$(sha256sum ${BUILD_DIR}/${IMAGE_NAME}.zip | awk '{ print $1 }') \
   && echo_stamp "Meashuring copmlete!" "SUCCESS"
 
   echo ""
-  echo "\$6: $6"
+  echo "\$5: $5"
   echo ""
 
   echo_stamp "Post message to GH"
-  local NEW_RELEASE_BODY="### Download\n* [$2.zip]($IMAGE_LINK) ($IMAGE_SIZE)\nsha256: $IMAGE_HASH\n\n$6"
+  local NEW_RELEASE_BODY="### Download\n* [${IMAGE_NAME}.zip]($IMAGE_LINK) ($IMAGE_SIZE)\nsha256: $IMAGE_HASH\n\n$5"
   local DATA="{ \"body\":\"$NEW_RELEASE_BODY\" }"
 
   echo ""
   echo "\$DATA: $DATA"
   echo ""
 
-  local GH_LOGIN=$(cat $4 | jq '.github.login' -r)
-  local GH_PASS=$(cat $4 | jq '.github.password' -r)
-  local GH_URL=$(cat $4 | jq '.github.url' -r)
-  curl -d "$DATA" -u "$GH_LOGIN:$GH_PASS" --request PATCH $GH_URL$5 \
+  local GH_LOGIN=$(cat $3 | jq '.github.login' -r)
+  local GH_PASS=$(cat $3 | jq '.github.password' -r)
+  local GH_URL=$(cat $3 | jq '.github.url' -r)
+  curl -d "$DATA" -u "$GH_LOGIN:$GH_PASS" --request PATCH $GH_URL$4 \
   && echo_stamp "Post message to GH copmlete!" "SUCCESS"
 }
 
@@ -292,8 +295,8 @@ case "$1" in
     resize_fs $2 $3;;
 
   publish_image)
-  # publish_image <BUILD_DIR> <IMAGE_NAME> <YA_SCRIPT> <CONFIG_FILE> <RELEASE_ID> <RELEASE_BODY>
-    publish_image $2 $3 $4 $5 $6 "$7";;
+  # publish_image <IMAGE_PATH> <YA_SCRIPT> <CONFIG_FILE> <RELEASE_ID> <RELEASE_BODY>
+    publish_image $2 $3 $4 $5 "$6";;
 
   execute)
   # execute <IMAGE> [<EXECUTE_FILE>] [...]
