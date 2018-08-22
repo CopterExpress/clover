2f584fd2d714cdad80923960b1eebc56e37ea83f
Artem Smirnov <urpylka@gmail.com>
2018-08-22 23:59:16 +0300

    image_builder: disable STDOUT in wget image

--- image_builder/image_config.sh
+++ image_builder/image_config.sh
@@ -36,7 +36,7 @@ get_image() {
   if [ ! -e "${BUILD_DIR}/${RPI_ZIP_NAME}" ];
   then
     echo_stamp "1. Downloading original Linux distribution"
-    wget -nv -O ${BUILD_DIR}/${RPI_ZIP_NAME} $2
+    wget -nv -O ${BUILD_DIR}/${RPI_ZIP_NAME} $2 > /dev/null
     echo_stamp "Downloading complete" "SUCCESS"
   else
     echo_stamp "1. Linux distribution already donwloaded"
