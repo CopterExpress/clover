2565997fb6afb7d23d6c27fd74cef7db9ef8f5b2
Artem Smirnov <urpylka@gmail.com>
2018-09-06 16:17:47 +0300

    image_builder: change key-storage

--- image_builder/scripts/ros_install.sh
+++ image_builder/scripts/ros_install.sh
@@ -32,7 +32,7 @@ echo_stamp "Installing ROS"
 
 echo_stamp "#1 Installing dirmngr & add key to apt-key"
 apt-get install --no-install-recommends -y -qq dirmngr=2.1.18-8~deb9u2 > /dev/null
-apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 echo "deb http://packages.ros.org/ros/ubuntu stretch main" > /etc/apt/sources.list.d/ros-latest.list
 
 echo_stamp "#2 apt update && apt upgrade"
