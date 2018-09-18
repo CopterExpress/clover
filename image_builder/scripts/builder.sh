815bebf6f2fedcaccc603352eb6e4afd46141a59
Artem Smirnov <urpylka@gmail.com>
2018-09-18 17:01:16 +0300

    image_builder: fix clever.rosinstall

--- image_builder/scripts/builder.sh
+++ image_builder/scripts/builder.sh
@@ -17,7 +17,7 @@ cd ${IMAGE_BUILDER}
 # If RPi then use a one thread to build a ROS package on RPi, else use all
 [[ $(arch) == 'armv7l' ]] && NUMBER_THREADS=1 || NUMBER_THREADS=$(nproc --all)
 
-./image_config.sh copy_to_chroot ${IMAGE_PATH} ${SCRIPTS_DIR}'/kinetic-ros-coex.rosinstall' '/home/pi/ros_catkin_ws/'
+./image_config.sh copy_to_chroot ${IMAGE_PATH} ${SCRIPTS_DIR}'/kinetic-ros-clever.rosinstall' '/home/pi/ros_catkin_ws/'
 ./image_config.sh execute ${IMAGE_PATH} ${SCRIPTS_DIR}'/ros_install.sh' ${TARGET_REPO} ${TARGET_REF} False False ${NUMBER_THREADS} 
 
 ./autosizer.sh ${IMAGE_PATH}
