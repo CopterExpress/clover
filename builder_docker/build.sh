f236db339239986778c9377f5ade4ef9af984f71
Artem Smirnov <urpylka@gmail.com>
2018-09-21 23:17:14 +0300

    image_builder: fix got TARGET_COMMIT

--- builder_docker/build.sh
+++ builder_docker/build.sh
@@ -29,7 +29,11 @@ git clone ${TARGET_REPO} --single-branch --branch ${TARGET_REF} --depth 1 ${REPO
 [[ -f ${REPO_DIR}${TARGET_CONFIG} ]] && export TARGET_CONFIG=${REPO_DIR}${TARGET_CONFIG} \
 || (echo "Error: TARGET_CONFIG doesn't exist!"; return 1)
 
-export IMAGE_VERSION="${TARGET_REF}_$(git show-ref --hash origin/master | cut -c1-7)"
+CUR_DIR="$(pwd)" && cd ${REPO_DIR}
+TARGET_COMMIT=$(git show-ref --hash origin/master | cut -c1-7)
+cd ${CUR_DIR} && unset CUR_DIR
+
+export IMAGE_VERSION="${TARGET_REF}_${TARGET_COMMIT}"
 export IMAGE_PATH="$(pwd)/image/$(basename -s '.git' ${TARGET_REPO})_${IMAGE_VERSION}.img"
 
 ./get_image.sh ${IMAGE_PATH} $(jq '.source_image' -r ${TARGET_CONFIG})
