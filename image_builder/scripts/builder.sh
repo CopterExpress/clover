2b1fd00ca024c5660f73505c0b68a701a11430fd
Artem Smirnov <urpylka@gmail.com>
2018-09-21 19:28:01 +0300

    image_builder: comment all stages for test gh release

--- image_builder/scripts/builder.sh
+++ image_builder/scripts/builder.sh
@@ -11,20 +11,20 @@ set -e
 cd ${IMAGE_BUILDER}
 
 # Make free space
-./resize_fs.sh ${IMAGE_PATH} '7G'
+# ./resize_fs.sh ${IMAGE_PATH} '7G'
 
-./image_config.sh copy_to_chroot ${IMAGE_PATH} ${SCRIPTS_DIR}'/init_rpi.sh' '/root/'
-./image_config.sh copy_to_chroot ${IMAGE_PATH} ${SCRIPTS_DIR}'/hardware_setup.sh' '/root/'
+# ./image_config.sh copy_to_chroot ${IMAGE_PATH} ${SCRIPTS_DIR}'/init_rpi.sh' '/root/'
+# ./image_config.sh copy_to_chroot ${IMAGE_PATH} ${SCRIPTS_DIR}'/hardware_setup.sh' '/root/'
 
-./image_config.sh execute ${IMAGE_PATH} ${SCRIPTS_DIR}'/init_image.sh' ${CLEVER_VERSION} $(jq '.source_image' -r ${TARGET_CONFIG})
-./image_config.sh execute ${IMAGE_PATH} ${SCRIPTS_DIR}'/software_install.sh'
-./image_config.sh execute ${IMAGE_PATH} ${SCRIPTS_DIR}'/network_setup.sh'
+# ./image_config.sh execute ${IMAGE_PATH} ${SCRIPTS_DIR}'/init_image.sh' ${CLEVER_VERSION} $(jq '.source_image' -r ${TARGET_CONFIG})
+# ./image_config.sh execute ${IMAGE_PATH} ${SCRIPTS_DIR}'/software_install.sh'
+# ./image_config.sh execute ${IMAGE_PATH} ${SCRIPTS_DIR}'/network_setup.sh'
 
-# If RPi then use a one thread to build a ROS package on RPi, else use all
-[[ $(arch) == 'armv7l' ]] && NUMBER_THREADS=1 || NUMBER_THREADS=$(nproc --all)
+# # If RPi then use a one thread to build a ROS package on RPi, else use all
+# [[ $(arch) == 'armv7l' ]] && NUMBER_THREADS=1 || NUMBER_THREADS=$(nproc --all)
 
-./image_config.sh copy_to_chroot ${IMAGE_PATH} ${SCRIPTS_DIR}'/kinetic-rosdep-clever.yaml' '/etc/ros/rosdep/'
-#./image_config.sh copy_to_chroot ${IMAGE_PATH} ${SCRIPTS_DIR}'/kinetic-ros-clever.rosinstall' '/home/pi/ros_catkin_ws/'
-./image_config.sh execute ${IMAGE_PATH} ${SCRIPTS_DIR}'/ros_install.sh' ${TARGET_REPO} ${TARGET_REF} False False ${NUMBER_THREADS} 
+# ./image_config.sh copy_to_chroot ${IMAGE_PATH} ${SCRIPTS_DIR}'/kinetic-rosdep-clever.yaml' '/etc/ros/rosdep/'
+# #./image_config.sh copy_to_chroot ${IMAGE_PATH} ${SCRIPTS_DIR}'/kinetic-ros-clever.rosinstall' '/home/pi/ros_catkin_ws/'
+# ./image_config.sh execute ${IMAGE_PATH} ${SCRIPTS_DIR}'/ros_install.sh' ${TARGET_REPO} ${TARGET_REF} False False ${NUMBER_THREADS} 
 
-./autosizer.sh ${IMAGE_PATH}
+# ./autosizer.sh ${IMAGE_PATH}
