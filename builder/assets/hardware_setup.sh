f91b7742c3ecc685a0ed6642b2bc0bf38e5df8b5
Artem Smirnov <urpylka@gmail.com>
2018-09-30 19:40:24 +0300

    image_builder: new configure UART on RPi

--- builder/assets/hardware_setup.sh
+++ builder/assets/hardware_setup.sh
@@ -58,7 +58,8 @@ echo_stamp "#6 Turn on UART"
 # https://github.com/RPi-Distro/raspi-config/pull/75
 /usr/bin/raspi-config nonint do_serial 1
 /usr/bin/raspi-config nonint set_config_var enable_uart 1 /boot/config.txt
-/usr/bin/raspi-config nonint set_config_var dtoverlay pi3-miniuart-bt /boot/config.txt
+/usr/bin/raspi-config nonint set_config_var dtoverlay pi3-disable-bt /boot/config.txt
+systemctl disable hciuart.service
 
 # After adding to Raspbian OS
 # https://github.com/RPi-Distro/raspi-config/commit/d6d9ecc0d9cbe4aaa9744ae733b9cb239e79c116
