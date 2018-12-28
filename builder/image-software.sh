5c2c14ca2399dc2e799e91ba40fcd52ddfaaa4f4
Oleg Kalachev <okalachev@gmail.com>
2018-12-28 04:42:05 +0300

    image: add i2c-tools

--- builder/image-software.sh
+++ builder/image-software.sh
@@ -100,6 +100,7 @@ build-essential=12.3 \
 libffi-dev \
 monkey=1.6.9-1 \
 pigpio python-pigpio python3-pigpio \
+i2c-tools \
 && echo_stamp "Everything was installed!" "SUCCESS" \
 || (echo_stamp "Some packages wasn't installed!" "ERROR"; exit 1)
 
