e443da60c44db553aff6474fed57c575b3c2dfdd
sfalexrog <sfalexrog@gmail.com>
2019-02-08 15:49:25 +0300

    Try to cache initial Raspbian image

--- builder/image-build.sh
+++ builder/image-build.sh
@@ -60,7 +60,7 @@ get_image() {
 
   if [ ! -e "${BUILD_DIR}/${RPI_ZIP_NAME}" ]; then
     echo_stamp "Downloading original Linux distribution"
-    wget --progress=bar:force:noscroll -O ${BUILD_DIR}/${RPI_ZIP_NAME} $2
+    wget --progress=dot:giga -O ${BUILD_DIR}/${RPI_ZIP_NAME} $2
     echo_stamp "Downloading complete" "SUCCESS" \
   else echo_stamp "Linux distribution already donwloaded"; fi
 
