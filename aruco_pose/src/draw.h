101ae7a337f956fc62e5daf3edbfec29dba8ee72
Oleg Kalachev <okalachev@gmail.com>
2019-08-01 23:13:23 +0300

    Editorconfig-check fix

--- aruco_pose/src/draw.h
+++ aruco_pose/src/draw.h
@@ -4,6 +4,6 @@
 #include <opencv2/aruco.hpp>
 
 void _drawPlanarBoard(cv::aruco::Board *_board, cv::Size outSize, cv::OutputArray _img,
-                      int marginSize, int borderBits, bool drawAxis);
+                      int marginSize, int borderBits, bool drawAxis); // editorconfig-checker-disable-line
 void _drawAxis(cv::InputOutputArray image, cv::InputArray cameraMatrix, cv::InputArray distCoeffs,
                cv::InputArray rvec, cv::InputArray tvec, float length); // editorconfig-checker-disable-line
