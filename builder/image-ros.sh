bdd4c4b152cdbdfdc5a782310d5a892451cb0154
Oleg Kalachev <okalachev@gmail.com>
2019-08-29 18:24:18 +0300

    Remove ws281x from Clever package dependencies

--- builder/image-ros.sh
+++ builder/image-ros.sh
@@ -174,6 +174,7 @@ apt-get install -y --no-install-recommends \
     ros-kinetic-rosserial \
     ros-kinetic-usb-cam \
     ros-kinetic-vl53l1x \
+    ros-kinetic-ws281x \
     ros-kinetic-opencv3=3.3.19-0stretch \
     ros-kinetic-rosshow
 
