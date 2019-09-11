540428aefc87237ce6cee898f402e8a0fbae50fc
Alexey Rogachevskiy <sfalexrog@gmail.com>
2019-09-11 20:45:57 +0300

    builder: Add repo for standalone build

--- builder/standalone-install.sh
+++ builder/standalone-install.sh
@@ -8,6 +8,22 @@ apt install -y curl
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python ./get-pip.py
 
+# Step 1.5: Add deb.coex.tech to apt
+curl http://deb.coex.tech/aptly_repo_signing.key 2> /dev/null | apt-key add -
+echo "deb http://deb.coex.tech/ros xenial main" > /etc/apt/sources.list.d/coex.tech.list
+echo "yaml file:///etc/ros/rosdep/coex.yaml" > /etc/ros/rosdep/sources.list.d/99-coex.yaml
+cat <<EOF > /etc/ros/rosdep/coex.yaml
+led_msgs:
+  ubuntu:
+    xenial: ros-kinetic-led-msgs
+    bionic: ros-melodic-led-msgs
+  debian:
+    stretch: ros-kinetic-led-msgs
+    buster: ros-melodic-led-msgs
+EOF
+apt update
+rosdep update
+
 # Step 2: Run rosdep to install all dependencies
 cd /root/catkin_ws
 rosdep install --from-paths src --ignore-src -y
