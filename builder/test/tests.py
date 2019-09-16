e5a52f5e5d8f6d56c1ab4cf29e514823c51ebd55
Oleg Kalachev <okalachev@gmail.com>
2019-09-16 20:25:19 +0300

    image: add test for new package name (clover)

--- builder/test/tests.py
+++ builder/test/tests.py
@@ -15,6 +15,8 @@ from mavros_msgs.srv import CommandBool, CommandLong, SetMode
 
 from std_srvs.srv import Trigger
 from clever.srv import GetTelemetry, Navigate, NavigateGlobal, SetPosition, SetVelocity, \
+    SetAttitude, SetRates, SetLEDEffect  # old name
+from clover.srv import GetTelemetry, Navigate, NavigateGlobal, SetPosition, SetVelocity, \
     SetAttitude, SetRates, SetLEDEffect
 
 import tf2_ros
