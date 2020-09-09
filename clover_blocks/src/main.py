a1676ae913ce0fab2b21af4553f7995d2233905d
Oleg Kalachev <okalachev@gmail.com>
2020-09-09 03:42:36 +0300

    Implement stop service

--- clover_blocks/src/main.py
+++ clover_blocks/src/main.py
@@ -1,26 +1,53 @@
 #!/usr/bin/env python
+
 import rospy
-import sys
+import os, sys
 import subprocess
 import threading
-from std_msgs.msg import String
+import signal
+import re
+from std_msgs.msg import Bool
 from std_srvs.srv import Trigger
-from clover_blocks.srv import Run
+from clover_blocks.srv import Run, Load, Store
 
 rospy.init_node('clover_blocks')
+process = None
+running_lock = threading.Lock()
 
 
 def run(req):
-    rospy.loginfo('Run program')
-    subprocess.call([sys.executable, '-c', req.code])
-    print('finish')
-    return {'success': True}
+    if not running_lock.acquire(False):
+        return {'message': 'Already running'}
+
+    try:
+        global process
+        rospy.loginfo('Run program')
+        process = subprocess.Popen([sys.executable, '-c', req.code])
+
+        def wait_thread():
+            process.wait()
+            rospy.loginfo('Program terminated')
+            running_lock.release()
+
+        t = threading.Thread(target=wait_thread)
+        t.start()
+
+        return {'success': True}
+
+    except Exception as e:
+        running_lock.release()
+        return {'message': str(e)}
 
 
 def stop(req):
-    # TODO
-    rospy.loginfo('Stop program')
-    return {'message': 'Not implemented yet'}
+    global process
+    if process:
+        rospy.loginfo('Stop program')
+        process.send_signal(signal.SIGINT)
+        process = None
+        return {'success': True}
+    else:
+        return {'success': True, 'message': 'Program not running'}
 
 
 rospy.Service('~run', Run, run)
