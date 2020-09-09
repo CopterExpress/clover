173c8cbe3ad2c998c042c4aa1aecbb61d7e0e584
Alexey Rogachevskiy <sfalexrog@gmail.com>
2020-09-09 12:37:00 +0300

    standalone_install: Auto-select Python, ROS distro

--- builder/standalone-install.sh
+++ builder/standalone-install.sh
@@ -6,36 +6,39 @@ set -e
 apt update
 apt install -y curl
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python ./get-pip.py
+if [ "x${ROS_PYTHON_VERSION}" = "x3" ]; then
+  PYTHON=python3
+else
+  PYTHON=python
+fi
+
+${PYTHON} ./get-pip.py
 
 # Step 1.5: Add deb.coex.tech to apt
 curl http://deb.coex.tech/aptly_repo_signing.key 2> /dev/null | apt-key add -
 echo "deb http://deb.coex.tech/ros xenial main" > /etc/apt/sources.list.d/coex.tech.list
 echo "yaml file:///etc/ros/rosdep/coex.yaml" > /etc/ros/rosdep/sources.list.d/99-coex.list
+CODENAME=$(lsb_release -sc)
+
 cat <<EOF > /etc/ros/rosdep/coex.yaml
 led_msgs:
   ubuntu:
-    xenial: [ros-kinetic-led-msgs]
-    bionic: [ros-melodic-led-msgs]
-    focal: [ros-noetic-led-msgs]
-  debian:
-    stretch: [ros-kinetic-led-msgs]
-    buster: [ros-melodic-led-msgs]
+    ${CODENAME}: [ros-${ROS_DISTRO}-led-msgs]
 async_web_server_cpp:
   ubuntu:
-    focal: [ros-noetic-async-web-server-cpp]
+    ${CODENAME}: [ros-${ROS_DISTRO}-async-web-server-cpp]
 ros_pytest:
   ubuntu:
-    focal: [ros-noetic-ros-pytest]
+    ${CODENAME}: [ros-${ROS_DISTRO}-ros-pytest]
 tf2_web_republisher:
   ubuntu:
-    focal: [ros-noetic-tf2-web-republisher]
+    ${CODENAME}: [ros-${ROS_DISTRO}-tf2-web-republisher]
 web_video_server:
   ubuntu:
-    focal: [ros-noetic-web-video-server]
+    ${CODENAME}: [ros-${ROS_DISTRO}-web-video-server]
 ws281x:
   ubuntu:
-    focal: [ros-noetic-ws281x]
+    ${CODENAME}: [ros-${ROS_DISTRO}-ws281x]
 EOF
 apt update
 rosdep update
