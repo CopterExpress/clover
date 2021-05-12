96eae2d50c44c19cc718a115ac50243b2bc1e9da
Oleg Kalachev <okalachev@gmail.com>
2021-05-12 08:54:38 +0300

    image: build overlay for OV7251 camera on CAM0

--- builder/image-hardware.sh
+++ builder/image-hardware.sh
@@ -1,3 +1,7 @@
 #! /usr/bin/env bash
 
 set -ex
+
+echo "Build overlay for OV7251 camera on CAM0"
+
+dtc -I dts -O dtb -o /boot/overlays/ov7251cam0.dtbo /home/pi/catkin_ws/src/clover/builder/assets/ov7251cam0-overlay.dts
