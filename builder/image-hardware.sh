594733385f723e843eb2b08bd247b39bada9bab6
Oleg Kalachev <okalachev@gmail.com>
2021-05-18 14:58:20 +0300

    Add a TODO

--- builder/image-hardware.sh
+++ builder/image-hardware.sh
@@ -4,4 +4,6 @@ set -ex
 
 echo "Build overlay for OV7251 camera on CAM0"
 
+# TODO: check Raspberry Pi OS version /boot/issue.txt
+
 dtc -I dts -O dtb -o /boot/overlays/ov7251cam0.dtbo /home/pi/catkin_ws/src/clover/builder/assets/ov7251cam0-overlay.dts
