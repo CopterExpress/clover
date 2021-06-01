f51044f44e8c17155c86beb0c937433bdf0745db
Oleg Kalachev <okalachev@gmail.com>
2021-06-01 03:44:20 +0300

    CI: add new key for apt Fixing https://github.com/CopterExpress/clover/runs/2700356960#step:3:74

--- builder/standalone-install.sh
+++ builder/standalone-install.sh
@@ -3,6 +3,7 @@
 # Perform a "standalone install" in a Docker container
 set -e
 # Step 1: Install pip
+apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654 # https://github.com/osrf/docker_images/issues/535
 apt-get update
 apt-get install -y curl
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
