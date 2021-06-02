2b36de5dfec54fce78d787d31ecfe2dc82b55748
Oleg Kalachev <okalachev@gmail.com>
2021-06-02 07:00:18 +0300

    Don’t use ROS for QR recognition test

--- builder/test/test_qr.py
+++ builder/test/test_qr.py
@@ -2,6 +2,7 @@
 
 # Test QG recognition example
 # Should be synced with the documentation: /docs/en/camera.md, /docs/ru/camera.md
+# TODO: use real ROS topics
 
 import rospy
 from pyzbar import pyzbar
@@ -10,7 +11,7 @@ from sensor_msgs.msg import Image
 
 bridge = CvBridge()
 
-rospy.init_node('barcode_test')
+# rospy.init_node('barcode_test')
 
 # Image subscriber callback function
 def image_callback(data):
@@ -23,17 +24,19 @@ def image_callback(data):
         xc = x + w/2
         yc = y + h/2
         print("Found {} with data {} with center at x={}, y={}".format(b_type, b_data, xc, yc))
-    rospy.signal_shutdown('done')
+    # rospy.signal_shutdown('done')
 
-image_sub = rospy.Subscriber('main_camera/image_raw', Image, image_callback, queue_size=1)
+# image_sub = rospy.Subscriber('main_camera/image_raw', Image, image_callback, queue_size=1)
 
 # ==============================================================================
 # Publish test image
+# rospy.sleep(2)
 
-rospy.sleep(2)
 import cv2
 img = cv2.imread('qr.png')
-image_pub = rospy.Publisher('/main_camera/image_raw', Image, queue_size=1, latch=True)
-image_pub.publish(bridge.cv2_to_imgmsg(img, 'bgr8'))
+image_callback(bridge.cv2_to_imgmsg(img, 'bgr8'))
 
-rospy.spin()
+# image_pub = rospy.Publisher('/main_camera/image_raw', Image, queue_size=1, latch=True)
+# image_pub.publish(bridge.cv2_to_imgmsg(img, 'bgr8'))
+
+# rospy.spin()
