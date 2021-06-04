f8156f58152b322a938097809a632a16cfe8e29d
Oleg Kalachev <okalachev@gmail.com>
2021-06-04 07:13:11 +0300

    Revert cv_camera to 0.5.0

--- builder/image-ros.sh
+++ builder/image-ros.sh
@@ -90,7 +90,7 @@ echo_stamp "Installing OpenCV 4.2-compatible ROS packages"
 apt install -y --no-install-recommends \
 ros-${ROS_DISTRO}-compressed-image-transport=1.14.0-0buster \
 ros-${ROS_DISTRO}-cv-bridge=1.15.0-0buster \
-ros-${ROS_DISTRO}-cv-camera=0.5.1-0buster \
+ros-${ROS_DISTRO}-cv-camera=0.5.0-0buster \
 ros-${ROS_DISTRO}-image-publisher=1.15.3-0buster \
 ros-${ROS_DISTRO}-web-video-server=0.2.1-0buster
 apt-mark hold \
