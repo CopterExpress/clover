33a6dffb1f6a9c3889a85ca2ad1e58afcb6739bb
Oleg Kalachev <okalachev@gmail.com>
2021-08-23 22:42:47 +0300

    Make examples more verbose

--- clover/examples/flight_marker.py
+++ clover/examples/flight_marker.py
@@ -15,23 +15,23 @@ set_attitude = rospy.ServiceProxy('set_attitude', srv.SetAttitude)
 set_rates = rospy.ServiceProxy('set_rates', srv.SetRates)
 land = rospy.ServiceProxy('land', Trigger)
 
-# Take off and hover 1 m above the ground
+print('Take off and hover 1 m above the ground')
 navigate(x=0, y=0, z=1, frame_id='body', auto_arm=True)
 
 # Wait for 5 seconds
 rospy.sleep(5)
 
-# Fly 1 meter above ArUco marker 0
+print('Fly 1 meter above ArUco marker 0')
 navigate(x=0, y=0, z=1, frame_id='aruco_0')
 
 # Wait for 5 seconds
 rospy.sleep(5)
 
-# Fly to x=1 y=1 z=1 relative to ArUco markers map
+print('Fly to x=1 y=1 z=1 relative to ArUco markers map')
 navigate(x=1, y=1, z=1, frame_id='aruco_map')
 
 # Wait for 5 seconds
 rospy.sleep(5)
 
-# Perform landing
+print('Perform landing')
 land()
