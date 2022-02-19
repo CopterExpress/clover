dc5ffc250bf181b027c7be1275bf396e7d5a5f9e
Oleg Kalachev <okalachev@gmail.com>
2022-02-19 04:16:49 +0300

    Increase timeouts for aruco_pose test

--- aruco_pose/test/crash_opencv.py
+++ aruco_pose/test/crash_opencv.py
@@ -9,10 +9,10 @@ def node():
     return rospy.init_node('aruco_pose_opencv_crash', anonymous=True)
 
 def test_opencv_crashes_img01(node):
-    rospy.wait_for_message('aruco_detect_01/visualization', VisMarkerArray, timeout=5)
+    rospy.wait_for_message('aruco_detect_01/visualization', VisMarkerArray, timeout=20)
 
 def test_opencv_crashes_img02(node):
-    rospy.wait_for_message('aruco_detect_02/visualization', VisMarkerArray, timeout=5)
+    rospy.wait_for_message('aruco_detect_02/visualization', VisMarkerArray, timeout=20)
 
 def test_opencv_crashes_img03(node):
-    rospy.wait_for_message('aruco_detect_03/visualization', VisMarkerArray, timeout=5)
+    rospy.wait_for_message('aruco_detect_03/visualization', VisMarkerArray, timeout=20)
