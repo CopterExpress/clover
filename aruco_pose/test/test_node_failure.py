03854d2589e74f896929edd3a24fbd093db14075
Oleg Kalachev <okalachev@gmail.com>
2022-07-19 03:30:43 +0300

    Implement dynamic reconfiguration for aruco_map (#448)

--- aruco_pose/test/test_node_failure.py
+++ aruco_pose/test/test_node_failure.py
@@ -2,6 +2,7 @@ import rospy
 import pytest
 
 from visualization_msgs.msg import MarkerArray as VisMarkerArray
+from aruco_pose.msg import MarkerArray
 
 
 @pytest.fixture
@@ -9,5 +10,5 @@ def node():
     return rospy.init_node('aruco_pose_test', anonymous=True)
 
 def test_node_failure(node):
-    with pytest.raises(rospy.exceptions.ROSException):
-        rospy.wait_for_message('aruco_map/visualization', VisMarkerArray, timeout=5)
+    assert rospy.wait_for_message('aruco_map/visualization', VisMarkerArray, timeout=5).markers == []
+    assert rospy.wait_for_message('aruco_map/map', MarkerArray, timeout=5).markers == []
