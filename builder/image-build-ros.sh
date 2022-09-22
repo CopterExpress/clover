0ef26334dce0d3fb720e7ac2d94bed10f372f7a6
Oleg Kalachev <okalachev@gmail.com>
2022-09-23 02:45:52 +0300

    Fix

--- builder/image-build-ros.sh
+++ builder/image-build-ros.sh
@@ -23,8 +23,8 @@ export ROS_OS_OVERRIDE=debian:$VERSION_CODENAME
 echo "=== Building ROS from scratch"
 
 echo "--- Adding sources"
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
 apt-get update
 apt-get install -y python3-distutils python3-rosdep python3-rosinstall-generator build-essential # python3-vcstool
