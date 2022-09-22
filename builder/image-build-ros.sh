a4ad408228bc08a1a1aecb0e6135f7b5e232c842
Oleg Kalachev <okalachev@gmail.com>
2022-09-23 00:09:09 +0300

    Restore rosdep update

--- builder/image-build-ros.sh
+++ builder/image-build-ros.sh
@@ -29,7 +29,7 @@ curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python3 get-pip.py &&
 pip3 install -U vcstool
 
 # sudo rosdep init
-# rosdep update
+rosdep update
 
 # rm /etc/ros/rosdep/sources.list.d/20-default.list && rosdep init
 # rosdep --os=debian:$VERSION_CODENAME update
