8d83e8faa3c39df031512a36ba974e81a68c50ce
Oleg Kalachev <okalachev@gmail.com>
2022-09-23 02:36:01 +0300

    Add ROS sources for Ubuntu

--- builder/image-build-ros.sh
+++ builder/image-build-ros.sh
@@ -22,6 +22,10 @@ export ROS_OS_OVERRIDE=debian:$VERSION_CODENAME
 
 echo "=== Building ROS from scratch"
 
+echo "--- Adding sources"
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+
 apt-get update
 apt-get install -y python3-distutils python3-rosdep python3-rosinstall-generator build-essential # python3-vcstool
 
