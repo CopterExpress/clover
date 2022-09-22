ad1d51fd9e1b031c8627eb4534b24a3b009e2530
Oleg Kalachev <okalachev@gmail.com>
2022-09-23 00:42:20 +0300

    Fix

--- builder/image-build-ros.sh
+++ builder/image-build-ros.sh
@@ -18,6 +18,7 @@ set -ex # exit on error, echo commands
 
 ROS_DISTRO=noetic
 . /etc/os-release # set $VERSION_CODENAME to Debian release code name
+export ROS_OS_OVERRIDE=$VERSION_CODENAME
 
 echo "=== Building ROS from scratch"
 
@@ -29,7 +30,7 @@ curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python3 get-pip.py &&
 pip3 install -U vcstool
 
 # sudo rosdep init
-rosdep update --os=debian:$VERSION_CODENAME
+rosdep update
 
 # rm /etc/ros/rosdep/sources.list.d/20-default.list && rosdep init
 # rosdep --os=debian:$VERSION_CODENAME update
