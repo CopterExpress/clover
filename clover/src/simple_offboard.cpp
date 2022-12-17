758fd204f689a192b6e9a4c5abc52f1cb130e629
Oleg Kalachev <okalachev@gmail.com>
2022-12-17 13:52:38 +0300

    Bring back mistakenly remove line

--- clover/src/simple_offboard.cpp
+++ clover/src/simple_offboard.cpp
@@ -913,7 +913,7 @@ bool serve(enum setpoint_type_t sp_type, float x, float y, float z, float vx, fl
 
 		wait_armed = auto_arm;
 
-		// publish setpoint
+		publish(stamp); // calculate initial transformed messages first
 		setpoint_timer.start();
 
 		if (setpoint_type == NAVIGATE || sp_type == NAVIGATE_GLOBAL || sp_type == POSITION) {
