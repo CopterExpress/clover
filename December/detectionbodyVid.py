50141188dec2593951a49ecb321f8ba79a04413a
Sarvar00 <59159103+Sarvar00@users.noreply.github.com>
2022-12-31 14:22:21 +0600

    Update detectionbodyVid.py

--- December/detectionbodyVid.py
+++ December/detectionbodyVid.py
@@ -1,23 +1,13 @@
 import numpy as np
 import cv2
-
-
-
 fullbody_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml') 
 upperBody_cascade = cv2.CascadeClassifier('haarcascade_upperbody.xml') 
 img = cv2.VideoCapture('waterrr.mp4')
-
-
-
 while True:
     ret, frames =img.read()
     gray= cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
-
     upper = upperBody_cascade.detectMultiScale(gray, 1.1, 1)
     full = fullbody_cascade.detectMultiScale(gray, 1.1 , 2)
-
-    
-
     for (x,y,w,h) in upper:
         cv2.rectangle(frames, (x,y), (x+w, y+h), (0,0,255),2)
     for (x,y,w,h) in full:
