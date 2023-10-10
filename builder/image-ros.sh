76982dc198e01a149877fee7a217db5ad7f503bb
Oleg Kalachev <okalachev@gmail.com>
2023-10-10 08:43:20 +0300

    image: install nodelet-topic-tools package

--- builder/image-ros.sh
+++ builder/image-ros.sh
@@ -138,7 +138,8 @@ my_travis_retry apt-get install -y --no-install-recommends \
     ros-${ROS_DISTRO}-rosshow \
     ros-${ROS_DISTRO}-cmake-modules \
     ros-${ROS_DISTRO}-image-view \
-    ros-${ROS_DISTRO}-image-geometry
+    ros-${ROS_DISTRO}-image-geometry \
+    ros-${ROS_DISTRO}-nodelet-topic-tools
 
 # TODO move GeographicLib datasets to Mavros debian package
 echo_stamp "Install GeographicLib datasets (needed for mavros)" \
