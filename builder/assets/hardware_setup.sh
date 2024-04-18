dc2293e960cb90e3ff2c39299a3294fc163ad42f
Oleg Kalachev <okalachev@gmail.com>
2024-04-18 06:51:11 +0300

    More build scripts cleanup

--- builder/assets/hardware_setup.sh
+++ builder/assets/hardware_setup.sh
@@ -19,27 +19,21 @@ set -e # Exit immidiately on non-zero result
 # Configure hardware interfaces
 ##################################################
 
-# 1. Enable sshd
 echo "--- Enable sshd"
 touch /boot/ssh
 # /usr/bin/raspi-config nonint do_ssh 0
 
-# 2. Enable GPIO
 echo "--- GPIO enabled by default"
 
-# 3. Enable I2C
 echo "--- Enable I2C"
 /usr/bin/raspi-config nonint do_i2c 0
 
-# 4. Enable SPI
 echo "--- Enable SPI"
 /usr/bin/raspi-config nonint do_spi 0
 
-# 5. Enable raspicam
 echo  "--- Enable raspicam"
 /usr/bin/raspi-config nonint do_camera 0
 
-# 6. Enable hardware UART
 echo "--- Enable UART"
 # Temporary solution
 # https://github.com/RPi-Distro/raspi-config/pull/75
@@ -52,9 +46,9 @@ systemctl disable hciuart.service
 # https://github.com/RPi-Distro/raspi-config/commit/d6d9ecc0d9cbe4aaa9744ae733b9cb239e79c116
 #/usr/bin/raspi-config nonint do_serial 2
 
-# 7. Enable V4L driver http://robocraft.ru/blog/electronics/3158.html
-#echo "bcm2835-v4l2" >> /etc/modules
 echo "--- Enable v4l2 driver"
+# http://robocraft.ru/blog/electronics/3158.html
+#echo "bcm2835-v4l2" >> /etc/modules
 if ! grep -q "^bcm2835-v4l2" /etc/modules;
 then printf "bcm2835-v4l2\n" >> /etc/modules
 fi
