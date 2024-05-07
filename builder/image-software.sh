8c8fdcda54736a6af54971cdb5c8272688508ba7
Oleg Kalachev <okalachev@gmail.com>
2024-05-07 21:33:06 +0300

    Try to fix Butterfly installation

--- builder/image-software.sh
+++ builder/image-software.sh
@@ -110,6 +110,9 @@ mv /usr/lib/python3.11/EXTERNALLY-MANAGED /usr/lib/python3.11/EXTERNALLY-MANAGED
 
 echo "--- Install and enable Butterfly (web terminal)"
 export CRYPTOGRAPHY_DONT_BUILD_RUST=1
+my_travis_retry pip3 install cryptography==3.4.6 # https://stackoverflow.com/a/68472128/6850197
+my_travis_retry pip3 install pyOpenSSL==20.0.1
+my_travis_retry pip3 install tornado==5.1.1
 my_travis_retry pip3 install butterfly
 my_travis_retry pip3 install butterfly[systemd]
 systemctl enable butterfly.socket
